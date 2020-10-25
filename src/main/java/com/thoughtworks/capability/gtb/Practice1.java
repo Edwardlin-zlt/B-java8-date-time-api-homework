ae520f89d8cdb90c8994548c6c6fa754f00f2845
lintao <lintao.zhang@thoughtworks.com>
2020-10-25 13:32:44 +0800

    [Lintao] feat: done

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -10,7 +11,16 @@ import java.time.LocalDate;
  */
 public class Practice1 {
 
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+        LocalDate laborDayOfSameYear = LocalDate.of(date.getYear(), 5, 1);
+        long between;
+
+        if (date.isAfter(laborDayOfSameYear)) {
+            LocalDate laborDayOfNextYear = laborDayOfSameYear.withYear(date.getYear() + 1);
+            between = ChronoUnit.DAYS.between(laborDayOfNextYear, date);
+        } else {
+            between = ChronoUnit.DAYS.between(laborDayOfSameYear, date);
+        }
+        return Math.abs(between);
+    }
 }
